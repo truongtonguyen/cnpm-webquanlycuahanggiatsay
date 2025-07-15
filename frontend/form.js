5c102083ff7c501f3b96cc3dc2f2901d81f01b0c
Bich_Ngoc <116694015+BichNgoc148@users.noreply.github.com>
2025-07-16 01:21:20 +0700

    Add files via upload
    Cập nhật thêm lien_he

--- frontend/form.js
+++ frontend/form.js
@@ -23,3 +23,39 @@ document.addEventListener("DOMContentLoaded", function () {
   });
 });
 
+/*==== LIÊN HỆ ====*/
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.getElementById("formGiatUi");
+  const popup = document.getElementById("successPopup");
+  const btnClose = document.getElementById("closePopup");
+
+  form.addEventListener("submit", function (e) {
+    e.preventDefault();
+
+    const formData = new FormData(form);
+
+    fetch("https://formsubmit.co/ajax/EMAIL_CUA_TIEP_NHAN@domain.com", {
+      method: "POST",
+      headers: {
+        "Accept": "application/json"
+      },
+      body: formData
+    })
+    .then(res => res.json())
+    .then(data => {
+      form.reset();
+      popup.classList.remove("hidden"); // ✅ hiện popup
+    })
+    .catch(err => {
+      alert("Gửi thất bại. Vui lòng thử lại!");
+      console.error(err);
+    });
+  });
+
+  btnClose.addEventListener("click", function () {
+    popup.classList.add("hidden"); // ✅ ẩn popup
+  });
+});
+
+
+
